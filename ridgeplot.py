e8a2cc45bc9110e1a976bc089fae8bceedde6499
Douglas Wu <wckdouglas@gmail.com>
2021-09-14 21:01:50 -0400

    added examples

--- ridgeplot.py
+++ ridgeplot.py
@@ -6,6 +6,9 @@ from more_itertools import first
 from scipy.stats import gaussian_kde
 
 
+class RidgePlotError(Exception):
+    pass
+
 def scaling(x: List[float]):
     """
     scaling a vector to a range between 0 and 1
@@ -23,8 +26,8 @@ def ridgeplot(
     ax: matplotlib.axes._axes.Axes,
     data: Dict[str, List[float]],
     xlim: Tuple[float, float] = None,
-    fill_color: str = "steelblue",
-    line_color: str = "white",
+    fill_colors: List[str] = None,
+    line_colors: List[str] = None,
     label_size: float = 10.0,
 ) -> None:
     """
@@ -33,8 +36,9 @@ def ridgeplot(
     :param matplotlib.axes._axes.Axes ax: a matplotlib ax object for writing the plot
     :param Dict data: data
     :param Tuple xlim: x-limits for the plot (xmin, xmax)
-    :param str fill_color: color for the fill under the distribution
-    :param str line_color: color for the line drawing the distribution
+    :param List[str] fill_colors: colors for the fill under the distribution, must be same length as input data (default: all steelblue)
+    :param List[str] line_colors: colors for the line drawing the distribution, must be same length as input data (default: all white)
+    :param float label_size: label size of the name of each distribution
 
 
     Example::
@@ -50,11 +54,28 @@ def ridgeplot(
         ax = fig.add_subplot(111)
         ridgeplot(ax, data, xlim=(-20,20))
     """
+
+    # assigning colors if not given
+    if fill_colors is None:
+        fill_colors = len(data) * ["steelblue"]
+
+    if line_colors is None:
+        line_colors = len(data) * ['white']
+
+    #assigning xlims if not given
     if xlim is not None:
         xmin, xmax = xlim
     else:
         xmin = min(first(data.values()))
         xmax = max(first(data.values()))
+
+    # data validation
+    if len(fill_colors) != len(data):
+        raise RidgePlotError("fill_colors must be same length as data")
+    
+    if len(line_colors) != len(data):
+        raise RidgePlotError("line_colors must be same length as data")
+
 
     xlines = []
     for sample_number, (data_key, data_values) in enumerate(data.items()):
@@ -64,8 +85,8 @@ def ridgeplot(
 
         baseline = sample_number * 0.7
         ys = scaling(kde.pdf(xs)) + baseline
-        ax.plot(xs, ys, color=line_color, lw=2)
-        ax.fill(xs, ys, color=fill_color)
+        ax.plot(xs, ys, color=line_colors[sample_number], lw=2)
+        ax.fill(xs, ys, color=fill_colors[sample_number])
         xlines.append(baseline)
         ax.text(xmin, baseline, data_key, ha="right", va="bottom", fontsize=label_size)
     ax.hlines(xlines, xmin=xmin, xmax=xmax * 1.1, color="black", lw=1)
