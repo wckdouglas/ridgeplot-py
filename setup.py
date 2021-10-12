acb0bdc2ac3944b600655317a37e58967106b6e0
Douglas Wu <wckdouglas@gmail.com>
2021-10-11 21:34:57 -0400

    modulized

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ridgeplot-py",
-    py_modules=["ridgeplot", "colors"],
+    py_modules=["ridgeplot",],
     install_requires=["numpy", "scipy", "matplotlib", "more_itertools"],
     python_requires=">3.6.1",
 )
--- test/test_colors.py
+++ test/test_colors.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 import pytest
 
-from colors import ColorEncoder, ColorPalette, check_color_vector_size, ordered_set
+from ridgeplot.colors import ColorEncoder, ColorPalette, check_color_vector_size, ordered_set
 
 
 @pytest.fixture(scope="module")
