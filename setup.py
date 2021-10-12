479050e3a332c898394ed1beab733640e10e07d9
Douglas Wu <wckdouglas@gmail.com>
2021-10-11 21:38:07 -0400

    modulized

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ridgeplot-py",
-    py_modules=["ridgeplot",],
+    packages=["ridgeplot",],
     install_requires=["numpy", "scipy", "matplotlib", "more_itertools"],
     python_requires=">3.6.1",
 )
