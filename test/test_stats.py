dd89b049b35e97d9bcb2bab88ba1e6eaddea59c5
Douglas Wu <wckdouglas@gmail.com>
2022-10-27 17:34:18 -0400

    refactored

--- test/test_stats.py
+++ test/test_stats.py
@@ -4,7 +4,7 @@ import pytest
 from ridgeplot.stats import scaling
 
 
-@pytest.parametrize("xs,expected", [([1, 2, 3, 4], [0, 0.333, 0.666, 1]), ([8, 6, 4, 2], [1, 0.66, 0.33, 0])])
+@pytest.mark.parametrize("xs,expected", [([1, 2, 3, 4], [0, 0.333, 0.666, 1]), ([8, 6, 4, 2], [1, 0.666, 0.333, 0])])
 def test_scaling(xs, expected):
     out = scaling(xs)
-    assert np.all(np.isclose(out, expected)), "Failed test"
+    assert np.all(np.isclose(out, expected, atol=0.001)), "Failed test"
